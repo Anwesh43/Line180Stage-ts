a21b116997adceef543babe3e0628bca1efc2a32
Anwesh43 <anweshthecool0@gmail.com>
2018-08-16 10:19:10 +0530

    adding Line180Node class

--- Line180Stage.ts
+++ Line180Stage.ts
@@ -75,3 +75,57 @@ class Animator {
         }
     }
 }
+
+class Line180Node {
+    prev : Line180Node
+    next : Line180Node
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new Line180Node(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        const index : number = this.i % 2
+        const factor : number = 1 - 2 * index
+        const gap : number = h / nodes
+        context.save()
+        context.translate(w / 2, h * (this.i + 1))
+        context.rotate(Math.PI * this.state.scale * factor)
+        context.fillStyle = '#FF5722'
+        context.fillRect(-gap/5, -gap, 2 * gap / 5, gap)
+        context.restore()
+        if (this.next) {
+            this.next.draw(context)
+        }
+    }
+
+    getNext(dir : number, cb : Function) : Line180Node {
+        var curr : Line180Node = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        if (cb) {
+            cb()
+        }
+        return this
+    }
+}
