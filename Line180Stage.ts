f46c07546e94f4a51b59f127fb064fb759040211
Anwesh43 <anweshthecool0@gmail.com>
2018-08-16 10:21:42 +0530

    adding LinkedLine180 class

--- Line180Stage.ts
+++ Line180Stage.ts
@@ -129,3 +129,25 @@ class Line180Node {
         return this
     }
 }
+
+class LinkedLine180 {
+    curr : Line180Node = new Line180Node(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
